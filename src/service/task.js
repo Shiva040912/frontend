f2aea82c10868d012eed247852cccb35e970e8e8
Shiva040912 <shivaarun0071@gmail.com>
2026-06-27 17:46:56 +0530

    fix: task adding

--- src/service/task.js
+++ src/service/task.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
-// பேஸ் URL மட்டும் வைப்போம்
+// 🚀 பேக்கண்ட் மெயின் URL (Render லிங்க்)
 const API = "https://auth-api-dnbf.onrender.com"; 
 
+// 1. டாஸ்க்குகளைக் கொண்டு வர (Get Tasks) - பாத்: /tasks/board/:boardId ஆக மாற்றப்பட்டுள்ளது
 export const getTasks = (boardId) =>
-  axios.get(`${API}/board/${boardId}`, {
+  axios.get(`${API}/tasks/board/${boardId}`, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
   });
 
+// 2. புது டாஸ்க் கிரியேட் பண்ண (Create Task)
 export const createTask = (data) =>
   axios.post(`${API}/tasks`, data, {
     headers: {
@@ -17,6 +19,7 @@ export const createTask = (data) =>
     },
   });
 
+// 3. டாஸ்க்கை அப்டேட் பண்ண (Update Task)
 export const updateTask = (id, data) =>
   axios.patch(`${API}/tasks/${id}`, data, {
     headers: {
@@ -24,6 +27,7 @@ export const updateTask = (id, data) =>
     },
   });
 
+// 4. டாஸ்க்கை டெலிட் பண்ண (Delete Task)
 export const deleteTask = (id) =>
   axios.delete(`${API}/tasks/${id}`, {
     headers: {
