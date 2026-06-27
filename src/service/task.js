025deadb2b5ad2bec38d03a42f99105a925d8961
Shiva040912 <shivaarun0071@gmail.com>
2026-06-27 17:21:34 +0530

    fix: board and vercel 404 issue fixed

--- src/service/task.js
+++ src/service/task.js
@@ -1,32 +1,31 @@
 import axios from "axios";
 
-// 🚀 '/tasks' எண்ட்-பாயிண்ட் சேர்க்கப்பட்டுள்ளது
-const API = "https://auth-api-dnbf.onrender.com/tasks"; 
+// பேஸ் URL மட்டும் வைப்போம்
+const API = "https://auth-api-dnbf.onrender.com"; 
 
 export const getTasks = (boardId) =>
-  axios.get(`https://auth-api-dnbf.onrender.com/board/${boardId}`, {
+  axios.get(`${API}/board/${boardId}`, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
   });
 
 export const createTask = (data) =>
-  axios.post(API, data, {
+  axios.post(`${API}/tasks`, data, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
   });
 
-// 💡 Drag and Drop செய்யும் போது இந்த updateTask தான் பேக்கண்டில் காலாகும்!
 export const updateTask = (id, data) =>
-  axios.patch(`${API}/${id}`, data, {
+  axios.patch(`${API}/tasks/${id}`, data, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
   });
 
 export const deleteTask = (id) =>
-  axios.delete(`${API}/${id}`, {
+  axios.delete(`${API}/tasks/${id}`, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
